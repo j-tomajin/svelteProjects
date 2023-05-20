8809b76427118b6ea5ffe768be3ebc44c50af8b5
Jerickson Tomajin <tomajinj@gmail.com>
2023-05-20 09:26:10 +0800

    src

---
+++ todo/src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Todo from './lib/Todo.svelte'
+
+    let todos = []
+	
+	const deleteTask = (id) => {
+		todos = todos.filter((todo) => todo.id != id)
+	}
+
+	const addNewTodo = (e) => {
+		const todo = e.detail
+		todos = [todo, ...todos]
+		console.log(todo.completed)
+	}	
+</script>
+
+<Todo on:addingNewTodo={addNewTodo} />
+
+<main>
+	{#each todos as todo (todo.id)}
+		<div class="todo">
+			<p>{todo.task}</p>
+			<button on:click={deleteTask(todo.id)}>delete</button>
+		</div>
+	{/each}
+</main>
+
+<style lang="scss">
+	.todo {
+		display: flex;
+		align-items: center;
+		gap: 24px;
+	}
+</style>
---
+++ todo/src/lib/Todo.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher()
+    
+    let id = 0
+    let task
+    let completed = false
+
+    function generateRandomNumber(x) {
+        return x = Math.floor(Math.random() * 100) + 1
+    }
+
+    function addNewTodo() {
+        const todo = {
+            id: generateRandomNumber(id),
+            task,
+            completed,
+        }
+
+        dispatch('addingNewTodo', todo)
+        task = ''
+    }
+</script>
+
+<h1>TODO</h1>
+
+<form on:submit|preventDefault={addNewTodo}>
+    <input type="text" bind:value={task} placeholder="create new todo">
+    <button type="submit">Add</button>
+</form>
+
+<style lang="scss">
+    h1 {
+        color: red;
+    }
+</style>
