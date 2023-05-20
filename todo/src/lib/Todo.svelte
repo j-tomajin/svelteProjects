68bf0e061ddc81fe055fb85f05530814ab7d2898
Jerickson Tomajin <tomajinj@gmail.com>
2023-05-20 20:56:06 +0800

    lib

--- todo/src/lib/Todo.svelte
+++ todo/src/lib/Todo.svelte
@@ -4,14 +4,34 @@
     const dispatch = createEventDispatcher()
     
     let id = 0
-    let task
+    let task = ''
     let completed = false
+
+    let selected
+    let options = [
+        {
+            id: 1,
+            option: 'All',
+            selected: true,
+        },
+        {
+            id: 2,
+            option: 'active',
+            selected: false,
+        },
+        {
+            id: 3,
+            option: 'completed',
+            selected: false,
+        }
+    ]
 
     function generateRandomNumber(x) {
         return x = Math.floor(Math.random() * 100) + 1
     }
 
     function addNewTodo() {
+        if(task == '') return
         const todo = {
             id: generateRandomNumber(id),
             task,
@@ -19,6 +39,7 @@
         }
 
         dispatch('addingNewTodo', todo)
+        dispatch('optionsDispatch', options)
         task = ''
     }
 </script>
@@ -27,11 +48,22 @@
 
 <form on:submit|preventDefault={addNewTodo}>
     <input type="text" bind:value={task} placeholder="create new todo">
+    
+    <select bind:value={selected}>
+        {#each options as option (option.id)}
+            <option value={option}>{option.option}</option>
+        {/each}
+    </select>
     <button type="submit">Add</button>
 </form>
 
 <style lang="scss">
+    input {
+        width: 50%;
+    }
+
     h1 {
-        color: red;
+        text-align: center;
+        color: rgb(0, 209, 209);
     }
 </style>
